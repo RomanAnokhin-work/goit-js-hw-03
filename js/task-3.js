3d31208271f4baa91a46004e70efd1ed41b73c33
Roman <roman.anokhin.work@gmail.com>
2025-07-22 17:29:50 +0300

    Update task 1-3

--- js/task-3.js
+++ js/task-3.js
@@ -1,9 +1,15 @@
-function getElementWidth(content, padding, border) {
-  const contentNumber = Number.parseFloat(content);
-  const paddingNumber = Number.parseFloat(padding);
-  const borderNumber = Number.parseFloat(border);
-  return contentNumber + paddingNumber * 2 + borderNumber * 2;
+function filterArray(numbers, value) {
+  const filteredNumbers = [];
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > value) {
+      filteredNumbers.push(numbers[i]);
+    }
+  }
+  return filteredNumbers;
 }
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
